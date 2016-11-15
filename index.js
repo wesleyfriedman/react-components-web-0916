ee3ecba639d6309a866515418b6c2e7d24d30249
wesleyfriedman <friedman.wesley@gmail.com>
2016-11-15 15:30:04 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const app = "I don't do much.";
+class Button extends React.Component {
+  render() {
+    return React.createElement('button', {}, 'Click me!');
+  }
+}
+
+const ShoppingList = React.createClass({
+  render() {
+    return React.createElement('ul', {}, [
+      React.createElement('li', {}, 'Bananas'),
+      React.createElement('li', {}, 'Vanilla ice cream'),
+      React.createElement('li', {}, 'Chocolate'),
+    ]);
+  }
+});
